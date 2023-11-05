ae43b67a3d2cd185ef2e09b21fcd8aaa1eadbf43
hayatelallaouy <hayatelallaouy01@gmail.com>
2023-11-05 19:54:02 +0100

    soap wsdl

---
+++ web_services_soap/src/main/java/ws/ServerJWS.java
@@ -0,0 +1,11 @@
+package ws;
+
+import jakarta.xml.ws.Endpoint;
+
+
+public class ServerJWS {
+    public static void main(String[] args) {
+        Endpoint.publish("http://0.0.0.0:9191/",new BanqueService());
+        System.out.println("Web service déployé sur http://localhost:9191/");
+    }
+}
